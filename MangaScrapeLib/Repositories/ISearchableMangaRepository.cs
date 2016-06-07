c43d52b2cf4de38c34ffc5e3b1f6c19b06469a59
claudiuslollarius <claudiuslollarius@gmail.com>
2016-06-07 16:28:23 +0900

    Refactoring

--- MangaScrapeLib/Repositories/ISearchableMangaRepository.cs
+++ MangaScrapeLib/Repositories/ISearchableMangaRepository.cs
@@ -1,10 +1,12 @@
 ﻿using MangaScrapeLib.Models;
+using System;
 using System.Collections.Generic;
 
 namespace MangaScrapeLib.Repositories
 {
     public interface ISearchableMangaRepository : IMangaRepository
     {
+        Uri GetSearchUri(string query);
         IEnumerable<SeriesInfo> GetSeriesFromSearch(string mangaSearchPageHtml);
     }
 }
