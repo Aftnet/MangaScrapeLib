f4bce1f78da8729ab43ebc4cad35947a365e42b6
claudiuslollarius <claudiuslollarius@gmail.com>
2016-07-07 16:55:39 +0900

    Tests

---
+++ MangaScrapeLib.Test/Models/ChapterTest.cs
@@ -0,0 +1,76 @@
+﻿using MangaScrapeLib.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MangaScrapeLib.Test.Models
+{
+    [TestClass]
+    public class ChapterTest
+    {
+        protected Series TestSeries = Series.CreateFromData(new Uri("http://eatmanga.com/Manga-Scan/Yamada-kun-to-7-nin-no-Majo/"), "SomeTitle");
+
+        [TestMethod]
+        public void CreateFromDataWorks()
+        {
+            var chapter = Chapter.CreateFromData(TestSeries, new Uri("http://eatmanga.com/Manga-Scan/Yamada-kun-to-7-nin-no-Majo/testch/"), "Test chapter");
+            Assert.IsNotNull(chapter);
+        }
+
+        [TestMethod]
+        public void CreateFromDataRejectsNullSeries()
+        {
+            var numExceptions = 0;
+            try
+            {
+                var chapter = Chapter.CreateFromData(null, new Uri("http://eatmanga.com/Manga-Scan/Yamada-kun-to-7-nin-no-Majo/testch/"), "Test chapter");
+            }
+            catch (ArgumentException)
+            {
+                numExceptions++;
+            }
+
+            Assert.AreEqual(1, numExceptions);
+        }
+
+        [TestMethod]
+        public void CreateFromDataRejectsUnsupportedUri()
+        {
+            var invalidUris = new Uri[] { null, new Uri("http://omg.lol/") };
+
+            var numExceptions = 0;
+            foreach (var i in invalidUris)
+            {
+                try
+                {
+                    var chapter = Chapter.CreateFromData(TestSeries, new Uri("http://omg.lol/"), "SomeTitle");
+                }
+                catch (ArgumentException)
+                {
+                    numExceptions++;
+                }
+            }
+
+            Assert.AreEqual(invalidUris.Length, numExceptions);
+        }
+
+        [TestMethod]
+        public void CreateFromDataRejectsInvalidTitle()
+        {
+            var invalidTitles = new string[] { null, string.Empty, "  " };
+            var numExceptions = 0;
+            foreach (var i in invalidTitles)
+            {
+                try
+                {
+                    var chapter = Chapter.CreateFromData(TestSeries, new Uri("http://eatmanga.com/Manga-Scan/Yamada-kun-to-7-nin-no-Majo/"), i);
+                }
+                catch (ArgumentNullException)
+                {
+                    numExceptions++;
+                }
+            }
+
+            Assert.AreEqual(invalidTitles.Length, numExceptions);
+        }
+    }
+}
