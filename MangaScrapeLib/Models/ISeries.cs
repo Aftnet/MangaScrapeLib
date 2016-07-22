bc6d23b432ae2d33d9e849df315a63f9550f0536
claudiuslollarius <claudiuslollarius@gmail.com>
2016-07-22 11:18:08 +0900

    Additional info to series

--- MangaScrapeLib/Models/ISeries.cs
+++ MangaScrapeLib/Models/ISeries.cs
@@ -9,6 +9,9 @@ namespace MangaScrapeLib.Models
         IRepository ParentRepository { get; }
         Uri SeriesPageUri { get; }
         Uri CoverImageUri { get; }
+        string Author { get; }
+        string Release { get; }
+        string Tags { get; }
         string Description { get; }
 
         Task<IChapter[]> GetChaptersAsync();
