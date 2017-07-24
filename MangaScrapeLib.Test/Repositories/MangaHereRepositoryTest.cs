83b2794ec274827ace54fb945b9520a13e2e6f71
Alberto Fustinoni <alberto@aftnet.net>
2017-07-24 22:24:08 +0900

    Ignore mangahere for now

--- MangaScrapeLib.Test/Repositories/MangaHereRepositoryTest.cs
+++ MangaScrapeLib.Test/Repositories/MangaHereRepositoryTest.cs
@@ -6,11 +6,11 @@ namespace MangaScrapeLib.Test.Repositories
     ///This is a test class for EatMangaRepositoryTest and is intended
     ///to contain all EatMangaRepositoryTest Unit Tests.
     ///</summary>
-    public class MangaHereRepositoryTest : MangaRepositoryTestBase
+    /*public class MangaHereRepositoryTest : MangaRepositoryTestBase
     {
         protected override Repository GetRepository()
         {
             return MangaHereRepository.Instance;
         }
-    }
+    }*/
 }
