aa442c2baaf0aff35f66001aaf6d80bfd143d85b
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-09-01 15:51:03 +0900

    Tests

--- MangaScrapeLib.Test/Repository/MangaHereRepositoryTest.cs
+++ MangaScrapeLib.Test/Repository/MangaHereRepositoryTest.cs
@@ -1,14 +1,7 @@
 ﻿namespace MangaScrapeLib.Test.Repository
 {
-    /// <summary>
-    ///This is a test class for EatMangaRepositoryTest and is intended
-    ///to contain all EatMangaRepositoryTest Unit Tests.
-    ///</summary>
-    /*public class MangaHereRepositoryTest : MangaRepositoryTestBase
+    public class MangaHereRepositoryTest : MangaRepositoryTestBase
     {
-        protected override Repository GetRepository()
-        {
-            return MangaHereRepository.Instance;
-        }
-    }*/
+        protected override IRepository Repository => Repositories.MangaHere;
+    }
 }
